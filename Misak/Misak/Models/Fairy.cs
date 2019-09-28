5939a4c98b5cbebb689c962367e465df3896e9fc
diegomisak <diegomisak@gmail.com>
2019-09-28 09:33:30 -0400

    Modelo Fairy Creado

--- Misak/Misak/Models/Fairy.cs
+++ Misak/Misak/Models/Fairy.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Misak.Models
-{
+{public enum WishList
+    { Desodorante,
+        Dulces,
+        Dalmata
+    }
     public class Fairy
     {
+        [Key]
+        public int FairyID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, MinimumLength = 2)]
+        public string NickName { get; set; }
+        public WishList Wishes { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Direccion de correo no valida")]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Cumpleaños")]
+        public DateTime Birthdate { get; set; }
+
+
     }
 }
